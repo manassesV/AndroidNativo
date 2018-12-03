d71a686e312138c2ba96a56a85a5002516f0a18e
Logon Aluno <logonrmlocal@fiap.com.br>
2018-12-03 19:22:54 -0200

    Initial commit

---
+++ app/src/main/java/com/example/logonrmlocal/myapplication/FormActivity.kt
@@ -0,0 +1,26 @@
+package com.example.logonrmlocal.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_form.*
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+
+
+        btCalculate.setOnClickListener( {
+            val proximatela = Intent(this@FormActivity, ResultActivity::class.java)
+            proximatela.putExtra("GAS_PRICE", etGasPrice.text.toString().toDouble())
+            proximatela.putExtra("ETHANOL_PRICE", etEthanolPrice.text.toString().toDouble())
+            proximatela.putExtra("GAS_AVERAGE", etGasAverage.text.toString().toDouble())
+            proximatela.putExtra("ETHANOL_AVERAGE",
+                    etEthanolAverage.text.toString().toDouble())
+            startActivity(proximatela)
+
+        })
+    }
+}
